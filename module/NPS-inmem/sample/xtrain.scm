dde91f52061784e83c77c61fe3df04feb25cd051
natsutan <YRW03704@nifty.com>
2015-11-11 17:06:49 +0900

    sampleの追加 readmeの追加

---
+++ module/NPS-inmem/sample/xtrain.scm
@@ -0,0 +1,12 @@
+; sample file
+(define *npsv-data-num* 100)    ; integer word length
+(define *npsv-init-file* "../sample/xtrain.dat")
+(define *npsv-W* 24)   ; total word length
+(define *npsv-I* 8)    ; integer word length
+(define *npsv-delta-T* 2)    ; integer word length
+
+(define *npsv-module-name* "xtrain")
+(define *npsv-rtl-output-dir* "../output/rtl")
+(define *npsv-init-output-dir* "../output/tb/init")
+(define *npsv-testbench-output-dir* "../output/tb")
+(define *npsv-template-output-dir* "../output/template")
