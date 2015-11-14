3d26784c9080901a5043cf526c4ff8c60f67260d
Minoru Natsutani <YRW03704@nifty.com>
2015-11-14 20:53:12 +0900

    NPS-rom追加

---
+++ module/NPS-rom/sample/parabola.scm
@@ -0,0 +1,13 @@
+; sample file
+(define *npsv-func* (lambda (x) (* x x)))
+(define *npsv-min* -5.0)
+(define *npsv-max* 5.0)
+(define *npsv-adr-width* 9)
+(define *npsv-W* 20)   ; total word length
+(define *npsv-I* 4)    ; integer word length
+
+(define *npsv-module-name* "paraborom")
+(define *npsv-rtl-output-dir* "../output/rtl")
+(define *npsv-testbench-output-dir* "../output/tb")
+(define *npsv-template-output-dir* "../output/template")
+
---
+++ module/NPS-rom/sample/tanh.scm
@@ -0,0 +1,13 @@
+; sample file
+(define *npsv-func* tanh)
+(define *npsv-min* -3.14)
+(define *npsv-max* 3.14)
+(define *npsv-adr-width* 9)
+(define *npsv-W* 24)   ; total word length
+(define *npsv-I* 8)    ; integer word length
+
+(define *npsv-module-name* "tanhrom")
+(define *npsv-rtl-output-dir* "../output/rtl")
+(define *npsv-testbench-output-dir* "../output/tb")
+(define *npsv-template-output-dir* "../output/template")
+
