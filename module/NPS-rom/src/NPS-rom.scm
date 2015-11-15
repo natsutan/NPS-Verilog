2baa6e68bd34d9efb6e5e846ebd507304ed6c260
Minoru Natsutani <YRW03704@nifty.com>
2015-11-15 15:45:20 +0900

    [NPS-rom] 入力ポートにadress typeを追加

--- module/NPS-rom/src/NPS-rom.scm
+++ module/NPS-rom/src/NPS-rom.scm
@@ -65,7 +65,7 @@
       (add-port inst (make <npsv-port> :name "vo" :dir 'output :type 'reg))
       (add-port inst (make <npsv-port> :name "fo" :dir 'output :type 'reg))
       (add-port inst (make <npsv-fixed-port> :name "datao" :dir 'output :msb (- W 1) :lsb 0 :fixed-info fixed :type 'reg))
-      (add-port inst (make <npsv-port> :name "datai" :dir 'input :msb (- adr-w 1) :lsb 0 ))
+      (add-port inst (make <npsv-port> :name "datai" :dir 'input :msb (- adr-w 1) :lsb 0 :type 'address))
       inst)))
 
 
