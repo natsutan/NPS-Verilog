1c154d13c344dcfef46dde9c0da19f854994410b
Minoru Natsutani <YRW03704@nifty.com>
2015-11-15 15:17:10 +0900

    [NPS-rom] sin波のサンプル対応

--- module/NPS-rom/src/NPS-rom.scm
+++ module/NPS-rom/src/NPS-rom.scm
@@ -30,7 +30,7 @@
       (dotimes(x num)
        (vector-set! data x (toFix (func (+ (* x unit) min)) W I)))
 ;      (print data)
-      data)))
+      (vector->list data))))
 
 
 ;;; --------------------------------------------------------------------------------
@@ -42,37 +42,18 @@
     (let ((format (- (power 2 dataw) 1)))
       (number->string (logand format data) 16))))
 
-(define make-rtl
-  (lambda (dir name data adrw dataw)
-    (let ((fp (open-verilog-file dir name)))
-      (make-header fp name)
-      (format fp "module ~A~%" name)
-      (format fp "\t(~%")
-      (format fp "\tinput CLK,~%")
-      (format fp "\tinput RESET_X,~%")
-      (format fp "\tinput \[~A:0\] ADR,~%" (- adrw 1))
-      (format fp "\toutput reg \[~A:0\] DATA~%" (- dataw 1))
-      (format fp "\t);~%")
-      (format fp "\talways @(posedge CLK or negedge RESET_X)begin~%")
-      (format fp "\t\tif(RESET_X == 1'b0)begin~%")
-      (format fp "\t\t\tDATA <= ~A'd0;~%" dataw)
-      (format fp "\t\tend else begin~%")
-      (format fp "\t\t\tcase(ADR)~%")
-      (dotimes (x (power 2 adrw))
-               (format fp "\t\t\t~A:DATA <= ~A'h~A;~%"
-                       x dataw (romdata->string (vector-ref data x) dataw)))
-      (format fp "\t\t\tendcase~%")
-      (format fp "\t\tend~%")
-      (format fp "\tend~%")
-      (format fp "endmodule~%")
-      (close-verilog-file fp))))
-
+(define insert-rom-data
+  (lambda (data dataw num)
+    (if (= (length data) 0)
+        ""
+        (string-append
+         (format #f "        ~A:datao <= ~A'h~A;~%"
+                 num dataw (romdata->string (car data) dataw))
+         (insert-rom-data (cdr data) dataw (+ num 1))))))
 
 ;;; --------------------------------------------------------------------------------
 ;;; verilog template
 ;;; --------------------------------------------------------------------------------
-
-
 (define make-instance
   (lambda (name min max adr-w W I)
     (let ([inst (make <npsv-module> :name name :type 'NPS-rom :comment "rom module")]
@@ -113,8 +94,116 @@
                  *npsv-I*))
   (make-verilog-file *instance* *npsv-rtl-output-dir*
                      (eval rtl-template (interaction-environment)))
+  (make-template *instance* *npsv-template-output-dir*)
+  (make-verilog-testbench-file *instance* *npsv-testbench-output-dir*
+                               (eval testbench-template (interaction-environment)))
+
+  0)
 
 
-    0)
+;;; --------------------------------------------------------------------------------
+;;; verilog source
+;;; --------------------------------------------------------------------------------
+(define rtl-template
+  '#"module ~*npsv-module-name* # (parameter DATA_WIDTH = ~*npsv-W* , ADR_WIDTH = ~(ceiling *npsv-adr-width*))
+  (
+    input 			clk,
+    input 			reset_x,
+    input 			start,
+    input 			set,
+    input 			vi,
+    input 			fi,
+    input [ADR_WIDTH-1:0] 	datai,   
+    output reg 			vo,
+    output reg 			fo,
+    output reg [DATA_WIDTH-1:0] datao
+   );
+  
+  always @(posedge clk or negedge reset_x)begin
+    if(reset_x == 1'b0)begin
+      datao <= 0;
+    end else begin
+      case(datai)
+~(insert-rom-data *data* *npsv-W* 0)
+    endcase
+    end
+  end // always @ (posedge clk or negedge reset_x)
 
+  always @ (posedge clk or negedge reset_x) begin
+    if(reset_x == 1'b0)begin
+      vo <= 0;
+      fo <= 0;
+    end else begin
+      vo <= vi;
+      fo <= fi;
+    end
+  end
+  
+endmodule
 
+")
+
+(define testbench-template
+  '#"
+module ~|*npsv-module-name*|_tb();
+  parameter DATA_WIDTH = ~*npsv-W*;
+  parameter ADR_WIDTH = ~*npsv-adr-width*;
+
+  reg clk;
+  reg reset_x;
+  reg set;
+  reg [ADR_WIDTH-1:0] datai;
+  wire [DATA_WIDTH-1:0] datao;
+  reg 			vi;
+  reg 			fi;
+  wire 			vo;
+  wire 			fo;
+
+  integer 		i;
+  integer 		fp;
+
+  parameter PERIOD = 10.0;
+  always # (PERIOD/2) clk = !clk;
+  initial begin clk = 1;end
+
+  ~*npsv-module-name* U0
+    (
+     .clk(clk),
+     .reset_x(reset_x),
+     .start(start),
+     .set(set),
+     .vi(vi),
+     .fi(fi),
+     .datai(datai),   
+     .vo(vo),
+     .fo(fo),
+     .datao(datao)
+     );
+
+  initial begin
+    fp=$fopen(\"~|*npsv-module-name*|_dump.txt\");
+    #1 reset_x = 1; datai = 0; fi = 0; set = 0; vi = 0;
+    # (PERIOD * 3)  reset_x = 0;
+    # (PERIOD * 5)  reset_x = 1;
+    
+    for(i=0;i<(2 ** ADR_WIDTH);i=i+1)begin
+      # (PERIOD) datai = i; vi = 1;
+    end
+    vi = 0;
+    
+    fi = 1;
+    @(posedge fo);
+    
+    # (PERIOD * 10)  
+    $fclose(fp);
+    $finish();
+  end // initial begin
+
+  always @ (posedge clk)begin
+    if(vo)begin
+      $fwrite(fp, \"%X\\n\", datao);
+    end
+  end
+  
+endmodule
+")
