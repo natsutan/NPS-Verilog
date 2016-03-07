1328e6b79ad71fefc5e22dd538d154979a599f34
natsutan <YRW03704@nifty.com>
2016-03-07 17:28:40 +0900

    add files

---
+++ module-sample/npsv-to-mtx/setting/to-mtx-1-2.scm
@@ -0,0 +1,14 @@
+; sample file
+
+(define *npsv-row* 2)   ; total word length
+(define *npsv-column* 3)    ; integer word length
+
+(define *npsv-W* 24)   ; total word length
+(define *npsv-I* 8)    ; integer word length
+
+(define *npsv-module-name* "to_2_3")
+(define *npsv-rtl-output-dir* "../output/rtl")
+(define *npsv-testbench-output-dir* "../output/tb")
+(define *npsv-template-output-dir* "../output/template")
+
+
